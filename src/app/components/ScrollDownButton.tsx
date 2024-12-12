0ad89d73d007d3311550f583ae63d0ca153e0bac
Rao Muhammad Akif Tufail <akifrao@gmail.com>
2024-12-12 16:31:16 +0500

    Update ScrollDownButton.tsx

--- src/app/components/ScrollDownButton.tsx
+++ src/app/components/ScrollDownButton.tsx
@@ -8,7 +8,7 @@ const ScrollDownButton: React.FC<ScrollDownButtonProps> = ({ onClick }) => {
   return (
     <button
       onClick={onClick}
-      className="fixed bottom-16 right-4 bg-white text-black p-3 rounded-full hover:bg-white transition-all"
+      className="fixed bottom-16 right-4 bg-white text-black px-3 py-2 rounded-full hover:bg-white transition-all"
     >
       <i className="fa-solid fa-arrow-down-long w-4 h-4"></i>
     </button>
