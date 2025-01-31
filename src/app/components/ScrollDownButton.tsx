40ee24f7ba33a0e39f52c4a8499f994242801cef
Rao Muhammad Akif Tufail <akifrao@gmail.com>
2025-01-31 22:47:19 +0500

    Add Scrolldown button to the center of the page

--- src/app/components/ScrollDownButton.tsx
+++ src/app/components/ScrollDownButton.tsx
@@ -9,7 +9,8 @@ const ScrollDownButton: React.FC<ScrollDownButtonProps> = ({ onClick }) => {
   return (
     <button
       onClick={onClick}
-      className="fixed bottom-16 right-4 bg-white text-black px-3 py-2 rounded-full hover:bg-white transition-all"
+      className="fixed bottom-32 left-1/2 transform -translate-x-1/2 bg-[#212121] text-white px-4 py-3 rounded-full transition-all shadow-lg border border-[#393636] flex items-center justify-center"
+      style={{ width: "30px", height: "30px", paddingTop: "13px" }}
       aria-label="Scroll Down"
     >
       <i className="fa-solid fa-arrow-down-long w-4 h-4"></i>
